593c53b538082d88ecb3e7805c7e73130a88c818
Daniel Tebbutt <moeruze.burning+git@gmail.com>
2013-03-30 01:02:16 +1300

    A couple of basic vector math unit tests

--- JavaGame/src/lasergame/vectormath/test/Vector2Test.java
+++ JavaGame/src/lasergame/vectormath/test/Vector2Test.java
@@ -13,8 +13,34 @@ public class Vector2Test {
 		Vector2 vecB = new Vector2(2.0f, 5.1f);
 		
 		vecA.add(vecB);
-		
+
 		assertEquals(vecA.x, 3.0f, 0.0001f);
+		assertEquals(vecA.y, 7.6f, 0.0001f);
+	}
+	
+	@Test
+	public void testSubtractVector() {
+		Vector2 vecA = new Vector2(1.0f, 2.5f);
+		Vector2 vecB = new Vector2(2.0f, 5.1f);
+		
+		vecA.subtract(vecB);
+		
+		assertEquals(vecA.x, -1.0f, 0.0001f);
+		assertEquals(vecA.y, -2.6f, 0.0001f);
+	}
+	
+	@Test
+	public void testMultiplyVector() {
+		Vector2 vecA = new Vector2(1.0f, 2.5f);
+		Vector2 vecB = new Vector2(-2.0f, 5.1f);
+		
+		vecA.multiply(2.5f);
+		vecB.multiply(-1.2f);
+
+		assertEquals(vecA.x, 2.5f, 0.0001f);
+		assertEquals(vecA.y, 6.25f, 0.0001f);
+		assertEquals(vecB.x, 2.4f, 0.0001f);
+		assertEquals(vecB.y, -6.12f, 0.0001f);
 	}
 
 }
